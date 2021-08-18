3c06599781579adbd86afc5fc56d12e99c870646
Cameron <cameron_uniac@hotmail.com>
2021-08-18 01:38:16 -0400

    Add solution for exercise seven

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,22 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Store < ActiveRecord::Base
+   validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+end
+
+class Employee < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+  validates :store_id, presence: true
+end
+
+puts "Enter a store name"
+store_name = gets.chomp
+store = Store.create(name: "#{store_name}")
+store.errors.messages.each do |var, message|
+  puts "#{var} #{message[0]}"
+end
