4551ebdedc3b98be0c1cbdf292e471cdf05ad16d
Cameron <cameron_uniac@hotmail.com>
2021-08-17 23:55:00 -0400

    Remove instruction comments

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -11,10 +11,3 @@ Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens
 
 stores = Store.all.count
 puts stores
-
-# Exercise 1: Create 3 stores
-# Use Active Record's create class method multiple times to create 3 stores in the database:
-# Burnaby (annual_revenue of 300000, carries men's and women's apparel)
-# Richmond (annual_revenue of 1260000 carries women's apparel only)
-# Gastown (annual_revenue of 190000 carries men's apparel only)
-# Output (puts) the number of the stores using ActiveRecord's count method, to ensure that there are three stores in the database.
